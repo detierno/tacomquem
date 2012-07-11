1d622fb3ca09b48e3f8d7f4be92e5b14b189bbb9
Denis <de.tierno@gmail.com>
2012-07-11 17:09:04 -0300

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Tacomquem::Application.config.secret_token = 'e61205215a450dbea291813ef02ee8398a4aaf6f60e0615e884a4bec0cb41c5a21d5d97dee3897bd33fd2437b09fb5c81c2edb5f68f6c70a48c437787416420a'
